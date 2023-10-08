f76e916788ee8047cd7b6e10fe8b262b000bc24d
Faith <thatsfaith@outlook.com>
2023-10-08 10:50:00 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const div = document.querySelector('div');
+
+
+function createButtons (){
+  for (let index = 0; index < 16; index++) {
+   const button = document.createElement('button');
+    div.appendChild(button);
+  }
+   
+}
+
+createButtons()
+
+
+function add (a, b){
+return a + b
+}
+
+
+function subtract (a, b){
+return a - b
+}
+
+
+function multiply (a, b){
+return a * b
+}
+
+
+function divide (a, b){
+return a / b
+}
+
+let numOne
+let operator
+let numTwo
+
+function operate (operator, a, b) {
+if (condition) {
+   
+}
+}
